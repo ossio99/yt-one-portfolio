0964a0dcd30d2d0ebbe458c7072e26e81cb2e50a
Eduardo Ossio Reyes <eduardoossio99@gmail.com>
2023-05-11 13:29:03 -0600

    experiencia pading top

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -63,7 +63,7 @@ const Experience = () => {
     return (
         <div
             name='experiencia'
-            className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'
+            className='bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-20'
         >
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
                 <div>
