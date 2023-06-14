f5565895cf42d3986123bc09063c99cca3ab6882
Eduardo Ossio Reyes <eduardoossio99@gmail.com>
2023-06-13 20:51:56 -0600

    bugs fixeds

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
-import installNode from '../assets/portfolio/installNode.jpg'
-import navBar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
-import reactWeather from '../assets/portfolio/reactWeather.jpg'
 import travelAgency from '../assets/portfolio/travelAgency.png'
 import appGastos from '../assets/portfolio/appGastos.png'
 
