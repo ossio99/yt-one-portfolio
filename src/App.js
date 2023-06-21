237d34730b262ce0cc134e725c5450724ded7789
Eduardo Ossio Reyes <eduardoossio99@gmail.com>
2023-06-21 17:28:05 -0600

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
 		<>
 			<HelmetProvider>
 				<Helmet>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+					{/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+					<meta name="description" content="Portafolio" />
 					<title>Ossio</title>
 				</Helmet>
 				<div className="App">
